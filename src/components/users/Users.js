36e9fa8a4891031fab5d907677920526e121a2a1
Noah Glaser <glaserpower@gmail.com>
2019-06-24 11:21:04 -0700

    first commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Spinner from '../layout/Spinner';
+import UserItem from "./UserItem";
+import PropTypes from 'prop-types';
+
+const Users = ({ users, loading }) => {
+
+    if (loading) {
+        return <Spinner />
+    }
+
+
+
+    return (
+      <div style={userStyle}>
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gap: "1rem"
+};
+
+export default Users;
